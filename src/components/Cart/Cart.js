95604dfbc365d1f549a72b23ca80b5a5b5d78c8c
smaran7713 <smaranmipl@gmail.com>
2022-05-20 21:07:32 +0600

    add to cat

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import './Cart.css';
 
-const cart = ({cart}) => {
+const cart = (props) => {
+    const { cart} = props;
+    let total = 0;
+    let shipping = 0;
+    let quantity = 0;
+    for(const product of cart){
+        quantity = quantity + product.quantity;
+        total = total + product.price * product.quantity;
+        shipping = shipping + product.shipping;
+    }
+    const tex = parseFloat((total * 0.1).toFixed(2));
+    const grandTotal = total + shipping + tex
+
     return (
-        <div>
+        <div className='cart'>
             <h4>Order Summary</h4>
-            <p>Selected Item: {cart.length}</p>
+            <p>Selected Item: {quantity}</p>
+            <p>Total Price: ${total}</p>
+            <p>Total Shipping: ${shipping}</p>
+            <p>Tax: ${tex}</p>
+            <h5>Grand Total: ${grandTotal.toFixed(2)}</h5>
         </div>
     );
 };
